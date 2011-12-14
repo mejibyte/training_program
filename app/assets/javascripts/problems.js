0dae42873fc38a5dbac97fdbd79e8dfa255b17ef
Andrés Mejía <andmej@gmail.com>
2011-12-13 20:04:50 -0500

    Do not slide to the top after opening notes

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -2,5 +2,6 @@ $(function(){
   $("a.toggle_notes").click(function(){
     console.log($(this).closest(".problem").find(".notes"));
     $(this).closest(".problem").find(".notes").slideToggle("fast");
+    return false;
   });
 });
